ba3ee0a588cf686eb641dd82dbca22797d60d595
Hope87 <vitaliy0187@gmail.com>
2021-06-04 11:59:45 +0300

    fixed revisions

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import male from '../../images/medium_male.png';
+import maleBig from '../../images/medium_male_big.png';
 import arrowLeft from '../../images/arrow_left.png';
 import arrowRight from '../../images/arrow_right.png';
 import style from './Sidebar.module.css';
@@ -8,7 +8,7 @@ const Sidebar = () => {
   return (
     <div className={style.candidateSidebar}>
       <div className={style.candidateSidebar__info}>
-        <img src={male} alt="" />
+        <img src={maleBig} alt="" />
         <p className={style.candidateSidebar__info__e}>Опыт работы</p>
         <p className={style.candidateSidebar__info__y}>4+ лет</p>
         <button className={style.candidateSidebar__info__btn}>Выбрать к собеседованию</button>
@@ -19,7 +19,7 @@ const Sidebar = () => {
         <div className={style.arrow__left}>
           <img src={arrowLeft} alt="" />
         </div>
-        <span className={style.arrows__sp}>1/15</span>
+        <span className={style.arrows__sp}>1 / 15</span>
         <div className={style.arrow__right}>
           <img src={arrowRight} alt="" />
         </div>
