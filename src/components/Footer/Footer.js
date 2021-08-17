d21ae2db20c150c61006f64387066a643b2c20fe
kurpfish <ph4z3r_f@mail.ru>
2021-08-17 13:16:51 +0300

    footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './footer.css';
+
+export const Footer = () => {
+    return (
+        <footer>
+            <div>2021 © Outstaffing</div>
+        </footer>
+    )
+}
