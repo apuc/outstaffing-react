d1f50e6d649dd639366877c0a4b17361ee30d5c4
M1kola <gigablock3030@yandex.ru>
2022-10-19 17:47:07 +0300

    fixes

--- src/components/features/quiz/HeaderQuiz.js
+++ src/components/features/quiz/HeaderQuiz.js
@@ -10,6 +10,7 @@ export const HeaderQuiz = ({header}) => {
    const dispatch = useDispatch()
    const userId = localStorage.getItem('id');
    const userInfo = useSelector(selectUserInfo);
+   console.log(userInfo)
 
    useEffect(() => {
       dispatch(setUserInfo(userId))
@@ -26,21 +27,25 @@ export const HeaderQuiz = ({header}) => {
    }, [dispatch])
 
    return (
-     <div className="header-quiz">
-        <div className="header-quiz__container">
-           {!userInfo ? <h2>Loading...</h2> :
-             <>
-                {header && <h2 className={'header-quiz__title-main'}>Добрый день, {userInfo.fio}</h2>}
-                <div className="header-quiz__body header-quiz__body_interjacent">
-                   <div className="header-quiz__avatar">
-                      <img src={userInfo.photo} alt={userInfo.photo}/>
-                   </div>
-                   <div className="header-quiz__name-user">{userInfo.fio}</div>
-                   <div className="header-quiz__title">{userInfo.position_name}</div>
-                </div>
-             </>
-           }
-        </div>
-     </div>
+       <div>
+       { userInfo?.status === 500 ? <div className="error-msg">{userInfo.message}</div> :
+         <div className="header-quiz">
+            <div className="header-quiz__container">
+               {!userInfo ? <h2>Loading...</h2> :
+                 <>
+                    {header && <h2 className={'header-quiz__title-main'}>Добрый день, {userInfo.fio}</h2>}
+                    <div className="header-quiz__body header-quiz__body_interjacent">
+                       <div className="header-quiz__avatar">
+                          <img src={userInfo.photo} alt={userInfo.photo}/>
+                       </div>
+                       <div className="header-quiz__name-user">{userInfo.fio}</div>
+                       <div className="header-quiz__title">{userInfo.position_name}</div>
+                    </div>
+                 </>
+               }
+            </div>
+         </div>
+       }
+       </div>
    )
 }
