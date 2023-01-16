50aae595e35c41c7d5deb6f3825d376313b25e7b
M1kola <gigablock3030@yandex.ru>
2023-01-16 16:39:33 +0300

    calendar and styles fixes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -34,7 +34,7 @@ export const Profile = () => {
                             <h3>Ваша отчетность</h3>
                         </div>
                         <div className='item__info'>
-                            <p><span>У вас 122 часа</span><br/> отработанных в этом месяце</p>
+                            <p><span></span>Отработанных в этом месяце</p>
                             <div className='item__infoLink'>
                                 <img src={rightArrow}  alt='arrow'/>
                             </div>
