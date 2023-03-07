8dbeb5bba55652a01ac870709e37d55c797dd007
M1kola <gigablock3030@yandex.ru>
2023-03-07 12:40:16 +0300

    tracker

--- src/pages/Tracker/Tracker.js
+++ src/pages/Tracker/Tracker.js
@@ -6,6 +6,10 @@ import {Footer} from '../../components/Footer/Footer'
 import project from '../../images/trackerProject.svg'
 import tasks from '../../images/trackerTasks.svg'
 import archive from '../../images/archiveTracker.svg'
+import avatarTest from '../../images/AvatarTest .png'
+import selectArrow from '../../images/select.svg'
+import commentsBoard from '../../images/commentsBoard.svg'
+import filesBoard from '../../images/filesBoard.svg'
 
 import './tracker.scss'
 
@@ -24,6 +28,92 @@ export const Tracker = () => {
         {
             name: 'Проект страхование',
             count: 4
+        }
+    ])
+    const [tabTaskMok] = useState([
+        {
+            name: 'Открытые',
+            tasks: [
+                {
+                    task: 'PR - 2245',
+                    description: 'Сверстать часть таблицы. Сверстать часть таблицы',
+                    comments: 12,
+                    files: 0,
+                    avatarCreated: avatarTest,
+                    avatarDo: avatarTest
+                },
+                {
+                    task: 'PR - 2245',
+                    description: 'Сверстать часть таблицы. Сверстать часть таблицы',
+                    comments: 12,
+                    files: 0,
+                    avatarCreated: avatarTest,
+                    avatarDo: avatarTest
+                }
+            ]
+        },
+        {
+            name: 'В процессе',
+            tasks: [
+                {
+                    task: 'PR - 2245',
+                    description: 'Сверстать часть таблицы. Сверстать часть таблицы',
+                    comments: 12,
+                    files: 0,
+                    avatarCreated: avatarTest,
+                    avatarDo: avatarTest
+                }
+            ]
+        },
+        {
+            name: 'На проверке',
+            tasks: [
+                {
+                    task: 'PR - 2245',
+                    description: 'Сверстать часть таблицы. Сверстать часть таблицы',
+                    comments: 12,
+                    files: 0,
+                    avatarCreated: avatarTest,
+                    avatarDo: avatarTest
+                },
+                {
+                    task: 'PR - 2245',
+                    description: 'Сверстать часть таблицы. Сверстать часть таблицы',
+                    comments: 12,
+                    files: 0,
+                    avatarCreated: avatarTest,
+                    avatarDo: avatarTest
+                },
+                {
+                    task: 'PR - 2245',
+                    description: 'Сверстать часть таблицы. Сверстать часть таблицы',
+                    comments: 12,
+                    files: 0,
+                    avatarCreated: avatarTest,
+                    avatarDo: avatarTest
+                }
+            ]
+        },
+        {
+            name: 'Готово',
+            tasks: [
+                {
+                    task: 'PR - 2245',
+                    description: 'Сверстать часть таблицы. Сверстать часть таблицы',
+                    comments: 12,
+                    files: 0,
+                    avatarCreated: avatarTest,
+                    avatarDo: avatarTest
+                },
+                {
+                    task: 'PR - 2245',
+                    description: 'Сверстать часть таблицы. Сверстать часть таблицы',
+                    comments: 12,
+                    files: 0,
+                    avatarCreated: avatarTest,
+                    avatarDo: avatarTest
+                }
+            ]
         }
     ])
 
@@ -65,6 +155,70 @@ export const Tracker = () => {
                                 })
                                 }
                                 <button><span>+</span>Создать проект</button>
+                            </div>
+                            <div className={toggleTab === 2 ? 'tracker__tabs__content__tasks tasks active__content' : 'tracker__tabs__content__projects'}>
+                                <div className='tasks__head'>
+                                    <h4>Проект : Разработка трекера</h4>
+                                    <span className='tasks__head__add'>+</span>
+                                    <div className='tasks__head__persons'>
+                                        <img src={avatarTest} alt='avatar' />
+                                        <img src={avatarTest} alt='avatar' />
+                                        <img src={avatarTest} alt='avatar' />
+                                        <img src={avatarTest} alt='avatar' />
+                                        <span>+9</span>
+                                    </div>
+                                    <div className='tasks__head__select'>
+                                        <span>Учавствую</span>
+                                        <img src={selectArrow} alt='arrow' />
+                                    </div>
+                                    <div className='tasks__head__select'>
+                                        <span>Мои</span>
+                                        <img src={selectArrow} alt='arrow' />
+                                    </div>
+                                </div>
+                                <div className='tasks__container'>
+                                    {tabTaskMok.map((section, index) => {
+                                            return <div key={index} className={section.tasks.length >= 3 ? 'tasks__board tasks__board__more' : 'tasks__board'}>
+                                                        <div className='board__head'>
+                                                            <span>{section.name}</span>
+                                                            <div>
+                                                                <span className='add'>+</span>
+                                                                <span className='more'>...</span>
+                                                            </div>
+                                                        </div>
+                                                        {section.tasks.map((task, index) => {
+                                                            return <div key={index} className='tasks__board__item'>
+                                                                        <div className='tasks__board__item__title'>
+                                                                            <p>{task.task}</p>
+                                                                            <span>...</span>
+                                                                        </div>
+                                                                        <p className='tasks__board__item__description'>
+                                                                            {task.description}
+                                                                        </p>
+                                                                        <div className='tasks__board__item__info'>
+                                                                            <div className='tasks__board__item__info__more'>
+                                                                                <img src={commentsBoard} alt='commentsImg' />
+                                                                                <span>{task.comments} коментариев</span>
+                                                                            </div>
+                                                                            <div className='tasks__board__item__info__more'>
+                                                                                <img src={filesBoard} alt='filesImg' />
+                                                                                <span>{task.files} файлов</span>
+                                                                            </div>
+                                                                            <div className='tasks__board__item__info__avatars'>
+                                                                                <img src={task.avatarCreated} alt='avatar' />
+                                                                                <img src={task.avatarDo} alt='avatar' />
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                        })
+                                                        }
+                                                        {section.tasks.length >= 3 &&
+                                                        <span className='moreItems'>+</span>
+                                                        }
+                                                    </div>
+                                        })
+                                    }
+                                </div>
                             </div>
                         </div>
                     </div>
