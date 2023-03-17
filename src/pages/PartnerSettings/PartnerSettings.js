3a65009ec91141f54ad0c1b2d16cc3bbc081e39e
MaxOvs19 <gshk3669@gmail.com>
2023-03-17 20:02:53 +0300

    Finished this page

--- src/pages/PartnerSettings/PartnerSettings.js
+++ src/pages/PartnerSettings/PartnerSettings.js
@@ -4,6 +4,8 @@ import { Footer } from "../../components/Footer/Footer";
 import { ProfileBreadcrumbs } from "../../components/ProfileBreadcrumbs/ProfileBreadcrumbs";
 import { ProfileHeader } from "../../components/ProfileHeader/ProfileHeader";
 
+import kontur from "../../images/konturLogo.png";
+import astral from "../../images/astralLogo.png";
 import "./partnerSettings.scss";
 
 export const PartnerSettings = () => {
@@ -22,6 +24,7 @@ export const PartnerSettings = () => {
           <div className="partner-settings__body">
             <div className="partner-settings__login">
               <h3 className="settings__title">Вход в систему</h3>
+
               <p className="settings__lable-first">Изменение логина</p>
               <div className="settings__input">
                 <input></input>
@@ -32,11 +35,11 @@ export const PartnerSettings = () => {
                 <input></input>
               </div>
 
-              <div>
-                <button>Отмена</button>
-                <button>Сохранить</button>
+              <div className="settings__buttons">
+                <button className="settings__buttons-cancel">Отмена</button>
+                <button className="settings__buttons-save">Сохранить</button>
               </div>
-              <span>
+              <span className="settings__agreement">
                 Нажимая "Сохранить", вы соглашаетесь с Правилами обработки и
                 использования персональных данных
               </span>
@@ -44,15 +47,36 @@ export const PartnerSettings = () => {
             <div className="partner-settings__report">
               <h3 className="settings__title">Документы и отчеты</h3>
               <p className="settings__lable-first">Изменить провадера ЭДО</p>
+
+              <div className="partner-settings__logo">
+                <div>
+                  <label className="partner-settings__label-first">
+                    <img src={astral}></img>
+                    <input type="checkbox" />
+                    <span className="checkbox__first"></span>
+                  </label>
+                </div>
+                <div>
+                  <label className="partner-settings__label-second">
+                    <img src={kontur}></img>
+                    <input type="checkbox" />
+                    <span className="checkbox__second"></span>
+                  </label>
+                </div>
+              </div>
+
+              <p className="settings__lable-second">
+                Изменение названия компании
+              </p>
               <div className="settings__input">
                 <input></input>
               </div>
 
-              <div>
-                <button>Отмена</button>
-                <button>Сохранить</button>
+              <div className="settings__buttons">
+                <button className="settings__buttons-cancel">Отмена</button>
+                <button className="settings__buttons-save">Сохранить</button>
               </div>
-              <span>
+              <span className="settings__agreement">
                 Нажимая "Сохранить", вы соглашаетесь с Правилами обработки и
                 использования персональных данных
               </span>
