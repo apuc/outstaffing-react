7ca123900249e894a1685eb7fb3d558ba04f8ed4
MaxOvs19 <gshk3669@gmail.com>
2023-03-20 13:05:27 +0300

    Finish update modal error

--- src/components/UI/ModalErrorLogin/ModalErrorLogin.js
+++ src/components/UI/ModalErrorLogin/ModalErrorLogin.js
@@ -2,8 +2,25 @@ import React from "react";
 
 import "./modalErrorLogin.scss";
 
-export const ModalErrorLogin = ({}) => {
-  return <div></div>;
+export const ModalErrorLogin = ({ active, setActive }) => {
+  return (
+    <div
+      className={active ? "modal-error active" : "modal-error"}
+      onClick={() => setActive(false)}
+    >
+      <div
+        className="modal-error__content"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <h2>Ошибка входа</h2>
+        <p>Введены некоректные данные для входа</p>
+        <button className="modal-error__content-button">
+          Попробовать еще раз
+        </button>
+        <span onClick={() => setActive(false)}></span>
+      </div>
+    </div>
+  );
 };
 
 export default ModalErrorLogin;
