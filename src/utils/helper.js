242f298bee1bdd99d0d85e76127eeb84f7a75ea6
M1kola <gigablock3030@yandex.ru>
2023-07-13 15:55:25 +0300

    task files

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -45,7 +45,7 @@ export const urlHasParams = (url) =>
 export const urlForLocal = (url) =>
   process.env.NODE_ENV === "development" ? `https://itguild.info${url}` : url;
 
-export const backendImg = (url) => `https://back.itguild.info${url}`
+export const backendImg = (url) => `https://back.itguild.info${url}`;
 
 export function scrollToForm() {
   window.scrollTo({
