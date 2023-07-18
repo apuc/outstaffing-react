fa582a8bfcc577235d0b23646c36c3d5652cc012
M1kola <gigablock3030@yandex.ru>
2023-07-18 16:20:55 +0300

    reset and confirm modals

--- src/components/Modal/ModalTrackerRegistration/ModalTrackerRegistration.js
+++ src/components/Modal/ModalTrackerRegistration/ModalTrackerRegistration.js
@@ -5,13 +5,24 @@ import close from "assets/icons/closeProjectPersons.svg";
 import "./modalTrackerRegistration.scss";
 
 export const ModalTrackerRegistration = ({ setModalReset }) => {
-    return (
-        <div className="modalConfirmTracker">
-            <h3 className='modalConfirmTracker__title'>Спасибо за регистрацию. Теперь можете войти в кабинет</h3>
-            <p className='modalConfirmTracker__info'>Мы отправили ссылку<br/>
-                для активации вашего аккаунта на почту<br/> <span>nhw44308@mail.com</span></p>
-            <button className='modalConfirmTracker__btn'>Перейти в почту</button>
-            <img onClick={() => setModalReset(false)} src={close} className='modalReset__close' alt='close' />
-        </div>
-    );
+  return (
+    <div className="modalConfirmTracker">
+      <h3 className="modalConfirmTracker__title">
+        Спасибо за регистрацию. Теперь можете войти в кабинет
+      </h3>
+      <p className="modalConfirmTracker__info">
+        Мы отправили ссылку
+        <br />
+        для активации вашего аккаунта на почту
+        <br /> <span>nhw44308@mail.com</span>
+      </p>
+      <button className="modalConfirmTracker__btn">Перейти в почту</button>
+      <img
+        onClick={() => setModalReset(false)}
+        src={close}
+        className="modalReset__close"
+        alt="close"
+      />
+    </div>
+  );
 };
--- src/pages/TrackerAuth/TrackerAuth.js
+++ src/pages/TrackerAuth/TrackerAuth.js
@@ -1,11 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import AuthBlock from "@components/AuthBlock/AuthBlock";
 import AuthHeader from "@components/Common/AuthHeader/AuthHeader";
 import { Footer } from "@components/Common/Footer/Footer";
-import SideBar from "@components/SideBar/SideBar";
 import ModalLayout from "@components/Common/ModalLayout/ModalLayout";
-import {ModalReset} from "@components/Modal/ModalReset/ModalReset";
+import { ModalReset } from "@components/Modal/ModalReset/ModalReset";
+import SideBar from "@components/SideBar/SideBar";
 
 import arrowInfo from "assets/icons/trackerIntroInfo.svg";
 import trackerAuthImg from "assets/images/trackerAuthImg.png";
@@ -13,7 +13,7 @@ import trackerAuthImg from "assets/images/trackerAuthImg.png";
 import "./trackerAuth.scss";
 
 export const TrackerAuth = () => {
- const [modalResetOpen, setModalReset] = useState(false)
+  const [modalResetOpen, setModalReset] = useState(false);
   return (
     <div className="trackerAuth">
       <AuthHeader />
@@ -33,11 +33,11 @@ export const TrackerAuth = () => {
             resetModal={setModalReset}
           />
         </div>
-          {modalResetOpen &&
-              <ModalLayout active={modalResetOpen} setActive={setModalReset}>
-                  <ModalReset setModalReset={setModalReset} />
-              </ModalLayout>
-          }
+        {modalResetOpen && (
+          <ModalLayout active={modalResetOpen} setActive={setModalReset}>
+            <ModalReset setModalReset={setModalReset} />
+          </ModalLayout>
+        )}
         <Footer />
       </div>
     </div>
