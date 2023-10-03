986c0833f7a17aba0787dd418b655e5fd192a47e
MaxOvs19 <gshk3669@gmail.com>
2023-10-03 18:03:32 +0300

    Finish page statistics

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -9,15 +9,48 @@ import { ProfileBreadcrumbs } from "@components/ProfileBreadcrumbs/ProfileBreadc
 import { ProfileHeader } from "@components/ProfileHeader/ProfileHeader";
 
 import arrow from "assets/icons/arrows/arrowCalendar.png";
+import emailImg from "assets/icons/emailStatistics.svg";
 import link from "assets/icons/link.svg";
 import project from "assets/icons/trackerProject.svg";
 import tasks from "assets/icons/trackerTasks.svg";
 import archive from "assets/images/archiveIcon.png";
+import mockAvatar from "assets/images/mock/AvatarTest .png";
 
 import "./statistics.scss";
 
 const Statistics = () => {
   const dispatch = useDispatch();
+
+  const teams = [
+    {
+      avatar: mockAvatar,
+      name: "Дмитрий Рогов",
+      email: "dmitryi.zavadskyi@yandex.ru",
+      role: "Программист",
+      status: true,
+    },
+    {
+      avatar: mockAvatar,
+      name: "Марина Орехова",
+      email: "dmitryi.zavadskyi@yandex.ru",
+      role: "Менеджер",
+      status: true,
+    },
+    {
+      avatar: mockAvatar,
+      name: "Тамара Доценко",
+      email: "dmitryi.zavadskyi@yandex.ru51515188151",
+      role: "Тестировщик",
+      status: false,
+    },
+    {
+      avatar: mockAvatar,
+      name: "Кек Лолов",
+      email: "dm4124gmail.com",
+      role: "PM",
+      status: false,
+    },
+  ];
 
   const toggleTabs = (index) => {
     dispatch(setToggleTab(index));
@@ -70,16 +103,94 @@ const Statistics = () => {
               <div className="statistics-header__menu">
                 <h1>Статистика проекта</h1>
                 <img src={link} alt="#" />
-                <span>ссылка на проект</span>
+                <span className="return-text">ссылка на проект</span>
               </div>
 
               <div className="statistics-header__return">
                 <img src={arrow} alt="#" />
-                <span>вернуться на все проекты</span>
+
+                <Link to={`/profile/tracker`} className="return-text">
+                  вернуться на все проекты
+                </Link>
               </div>
             </div>
-            <div className="statistics-info"></div>
-            <div className="statistics-team"></div>
+            <div className="statistics-info">
+              <div className="statistics-info__head">
+                <p>Проект: </p>
+                <h1>{"Разработка трекера"}</h1>
+              </div>
+              <div className="statistics-info__team">
+                <div className="project-info">
+                  <div className="project-info__creator">
+                    <span className="return-text">Создатель проекта:</span>
+                    <div>
+                      <p>{"Василий Тарасов"}</p>{" "}
+                      <img src={mockAvatar} alt="#" />
+                    </div>
+                  </div>
+                  <div className="project-info__tasks">
+                    <div className="task-quantity">
+                      <p>Открытые задачи</p>
+                      <span className="task-quantity_open">{4}</span>
+                    </div>
+                    <div className="task-quantity">
+                      <p>Задач в работе</p>
+                      <span className="task-quantity_work">{5}</span>
+                    </div>
+                    <div className="task-quantity">
+                      <p>Закрыто задач</p>
+                      <span className="task-quantity_closed">{434}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="list-team">
+                  <div className="list-team__title">
+                    <span className="return-text">Участники проекта:</span>
+                  </div>
+                  <div className="list-team__head">
+                    <p>Имя</p>
+                    <p>Почта</p>
+                    <p>Роль</p>
+                    <p>Статус</p>
+                  </div>
+                  <div className="list-team__body">
+                    {teams.map((item) => {
+                      return (
+                        <>
+                          <div className="list-team__item">
+                            <div className="person-name">
+                              <img src={item.avatar} alt="#" />
+                              <p>{item.name}</p>
+                            </div>
+                            <div className="person-email">
+                              <img src={emailImg} alt="#" />
+                              <p>{item.email}</p>
+                            </div>
+
+                            <p className="person-type">{item.role}</p>
+                            {/* <span className="status status-active"> */}
+
+                            <span
+                              className={
+                                item.status
+                                  ? "status status-active"
+                                  : "status status-none"
+                              }
+                            >
+                              {item.status ? "Активно" : "Не активно"}
+                            </span>
+                          </div>
+                        </>
+                      );
+                    })}
+                  </div>
+                </div>
+                <div className="add-person">
+                  <span className="add-person__button">+</span>
+                  <p>Добавить участников</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
